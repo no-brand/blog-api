0cfeb83f206fee4e130503ee4f9c5fedb9c5b6fe
no-brand <do.dream.david@gmail.com>
2022-12-17 12:00:00 +0900

    feat: response with @ControllerAdvice

--- src/main/java/com/nobrand/blogapi/controller/ExceptionController.java
+++ src/main/java/com/nobrand/blogapi/controller/ExceptionController.java
@@ -2,18 +2,35 @@ package com.nobrand.blogapi.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 @ControllerAdvice
 public class ExceptionController {
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(Exception.class)
-    public void exceptionHandler(Exception e) {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseBody
+    public Map<String, String> methodArgumentNotValidExceptionHandler(MethodArgumentNotValidException e) {
       log.warn(e.toString());
+
+      Map<String, String> response = new HashMap<>();
+      if (e.hasErrors()) {
+          for (FieldError fieldError: e.getFieldErrors()) {
+              String fieldName = fieldError.getField();
+              String errorMessage = fieldError.getDefaultMessage();
+              response.put(fieldName, errorMessage);
+          }
+      }
+      return response;
     }
 
 }
